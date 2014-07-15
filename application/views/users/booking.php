0c4c501f3f2b97013aca6a618ec22a345600a2bf
fikriimujahid <fikriimujahid@hotmail.com>
2014-07-16 01:04:14 +0700

    ganti nama

--- application/views/users/booking.php
+++
@@ -1,9 +0,0 @@
-<div class="formpinjam">
-<form method="post">
-    <input type="text" placeholder="Nomor Induk Siwa" name="nis" required>
-    <input type="text" placeholder="ID buku" name="idbukupinjam" required>
-    <label for="tglpinjam">Pinjam</label><input type="date" name="tglpinjam">
-    <label for="tglkembali">Kembali</label><input type="date" name="tglkembali">
-    <input type="submit" value="Submit" name="pinjam">
-</form>
-</div>
